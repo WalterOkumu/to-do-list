e367184129c0f4f40a3b8d8e691a50b1d1d24d1a
Okumu Oriaro <okumu.oriaro@gmail.com>
2022-08-22 23:38:54 +0300

    Corrections to delete function

--- src/components/deleteTask.js
+++ src/components/deleteTask.js
@@ -2,26 +2,25 @@ import storageAvailable from './storageAvailable.js';
 
 const deleteTask = () => {
   window.onclick = (event) => {
-    const { localStorage } = window;
+    const { localStorage, location } = window;
+    const itemIndex = parseInt(event.target.parentElement.id.replace('task-icon-', ''), 10);
 
     if (storageAvailable('localStorage')) {
       let taskList = [];
 
       taskList = JSON.parse(localStorage.getItem('taskList'));
 
-      const itemIndex = event.path[2].id.replace('task-icon-', '');
-
       taskList.splice(itemIndex, 1);
 
-      for (let i = 0; i < taskList.length; i += 1) {
-        taskList[i].index = i + 1;
-      }
+      taskList.forEach((task, index) => {
+        task.index = index + 1;
+      });
 
       localStorage.removeItem('taskList');
 
       localStorage.setItem('taskList', JSON.stringify(taskList));
 
-      window.location.reload();
+      location.reload();
     }
   };
 };
